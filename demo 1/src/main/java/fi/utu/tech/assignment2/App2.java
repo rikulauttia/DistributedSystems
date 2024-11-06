15daaaf7a8f6dfb2812f827453e99cd85e571f2a
Riku Lauttia <riku.lauttia@gmail.com>
2024-11-06 14:08:15 +0200

    teht 2 valmis

--- demo 1/src/main/java/fi/utu/tech/assignment2/App2.java
+++ demo 1/src/main/java/fi/utu/tech/assignment2/App2.java
@@ -11,6 +11,41 @@ import fi.utu.tech.common.SubmissionGenerator.Strategy;
 public class App2 {
     public static void main( String[] args )
     {
-        // Kopioi edellisen tehtävän ratkaisu tähän lähtökohdaksi
+        // Otetaan funktion aloitusaika talteen suoritusajan laskemista varten
+        long startTime = System.currentTimeMillis();
+
+        // Generoidaan kasa esimerkkitehtäväpalautuksia
+        List<Submission> ungradedSubmissions = SubmissionGenerator.generateSubmissions(21, 200, Strategy.STATIC);
+
+        // Tulostetaan tiedot esimerkkipalautuksista ennen arviointia
+        for (var ug : ungradedSubmissions) {
+            System.out.println(ug);
+        }
+
+        // Luodaan uusi gardintask olio ja annetaan sille arvioimattomat palautukset
+        GradingTask gradingTask = new GradingTask(ungradedSubmissions);
+
+        // Luodaan uusi säie ja annetaan sille tehtäväksi GradingTaskin suorittaminen
+        Thread gradingThread = new Thread(gradingTask);
+
+        // Käynnistetään säie
+        gradingThread.start();
+
+        // Odotetaan säikeen suorituksen päättymistä
+        try {
+            gradingThread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        List<Submission> gradedSubmissions = gradingTask.getGradedSubmissions();
+
+        // Tulostetaan arvioidut palautukset
+        System.out.println("------------ CUT HERE ------------");
+        for (var gs : gradedSubmissions) {
+            System.out.println(gs);
+        }
+
+        // Lasketaan funktion suoritusaika
+        System.out.printf("Total time for grading: %d ms%n", System.currentTimeMillis()-startTime);
     }
 }
